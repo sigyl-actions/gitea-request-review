f47ef7e6868f173ea85ab3cfd9b05780419b71dc
gilesb <giles.bradshaw@sigyl.com>
2023-11-01 15:05:53 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,164 @@
+import fs from 'fs';
+import { Blob } from 'buffer';
+
+
+import core from '@actions/core';
+import github from '@actions/github';
+
+import { GiteaApi } from 'gitea-api';
+
+async function run() {
+  try {
+    console.log(process.argv)
+    const serverUrl = core.getInput('serverUrl')
+      || (github.context.runId && github.context.serverUrl)
+      || 'https://sigyl.com/git'
+
+    const client = new GiteaApi({
+      BASE: `${serverUrl}/api/v1`,
+      WITH_CREDENTIALS: true,
+      TOKEN: core.getInput('token') || process.argv[2],
+    });
+    const [owner, repo] = (
+      core.getInput('repository')
+      || github?.context?.payload?.repository?.full_name
+      || 'actions/batch-example'
+    ).split("/");
+
+    const reviews = await client
+      .repository
+      .repoListPullReviews({
+        owner,
+        repo,
+        index: core.getInput('pr') || 69,
+      });
+
+    const orgTeams = await Promise.all((
+      await client
+        .organization
+        .orgListTeams({
+          org: owner,
+        })
+      ).map(
+        async (
+          team,
+        ) => ({
+          team,
+          members: await client
+            .organization
+            .orgListTeamMembers({
+              id: team.id,
+            })
+        })
+      ));
+    
+    const teams = core.getInput('teams') || [
+      "reviewers",
+      "reviewers-2"
+    ]
+    console.log(JSON.stringify(reviews, null, 2));
+    const teamReviews = teams
+      .map(
+        (team) => ({
+          team,
+          reviews: reviews
+            .filter(
+              ({
+                user,
+              }) => user,
+            )
+            .filter(
+              ({
+                state,
+                user: {
+                  id: userId,
+                }
+              }) => orgTeams
+                .find(
+                  ({
+                    team: {
+                      name,
+                    },
+                    members,
+                  }) => name === team
+                    && members
+                      .find(
+                        ({
+                          id: memberId
+                        }) => memberId === userId,
+                      )
+                ),
+            )
+        })
+      );
+
+    console.log(
+      JSON.stringify(
+        {
+          teamReviews,
+        },
+        null,
+        2,
+      )
+    );
+    const reviewRequests = teamReviews
+      .filter(
+        ({
+          reviews,
+        }) => reviews
+          .find(
+            ({
+              state,
+              dismissed,
+              stale,
+            }) => !dismissed && !stale && (state === 'REQUEST_REVIEW'
+              || state === 'REQUEST_CHANGES'),
+          )
+      );
+    const nonDismissedReviews = teamReviews.filter(
+      ({
+        reviews,
+      }) => !reviews
+        .filter(
+          ({
+            dismissed,
+            stale,
+          }) => !dismissed && !stale,
+        ).length
+    )
+    if (
+      !reviewRequests.length &&
+      nonDismissedReviews.length
+    ) {
+      console.log('requesting review for', nonDismissedReviews[0].team)
+      console.log(
+        JSON.stringify(
+          {
+            reviews,
+            teams,
+            api: await client.repository.repoCreatePullReviewRequests({
+              owner,
+              repo,
+              index: core.getInput('pr') || 69,
+              body: {
+                team_reviewers: [
+                  nonDismissedReviews[0].team,
+                ],
+              }
+            }),
+          },
+          null,
+          2
+        ),
+      );
+    } else {
+      console.log('nothing done')
+    }
+  }
+  catch (error) {
+    console.error(error)
+    core.setFailed(error.message);
+  }
+}
+
+run()
